1981f27bfb96ad4ac89e0cd91b9b025f6580ce18
mac503 <mac503@gmail.com>
2019-01-30 20:18:42 +0100

    Nicer format of booking confirmation page.

--- includes/pages/booking_request_confirmation.php
+++ includes/pages/booking_request_confirmation.php
@@ -6,9 +6,17 @@ function booking_request_confirmation(){
   $type = $_SESSION['DATA_'.$_GET['data_id']]['type'];
   $time = $_SESSION['DATA_'.$_GET['data_id']]['time'];
   $date = date("D d M", strtotime($_SESSION['DATA_'.$_GET['data_id']]['date']));
-
-  echo "$name, we will contact you to confirm your request for $type at $time on $date.<p><p>";
+?>
+  <div id='ss_booking_confirmation_text'>
+<?php
+  echo "$name, <b>we will contact you to confirm your request</b>:<br/><br/>";
+  echo "$type<br/>";
+  echo "$time on $date<br/><br/>";
   echo "Thank you.";
+?>
+  <br/><br/>Perhaps a picture here.<br/>
+  </div>
+<?php
   return ob_get_clean();
 }
 ?>
