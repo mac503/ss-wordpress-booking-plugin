baefadc6d619f830ec8a76a5595d33041c939b60
mac503 <mac503@gmail.com>
2019-01-30 20:22:07 +0100

    Search bar placeholder.

--- includes/pages/manage_bookings_page.php
+++ includes/pages/manage_bookings_page.php
@@ -18,9 +18,10 @@ function manage_bookings_page(){
     <div data-action='change-opening-hours'>Change Opening Hours</div>
   </div>
   <div id='ss_booking_item_dialog' style='display:none'></div>
+  <div class='overlay'></div>
 </div>
 <div id='ss_booking_list' style='display:none'>
-  <div id='ss_booking_list_search_bar'><input type='text' id='ss_booking_list_search_box'></input></div><p>
+  <div id='ss_booking_list_search_bar'><input type='text' id='ss_booking_list_search_box' placeholder='Search'></input></div><p>
   <div id='ss_booking_list_list'></div>
 </div>
 <?php
