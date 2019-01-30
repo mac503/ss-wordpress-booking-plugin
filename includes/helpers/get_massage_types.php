729506f33cc3b57759b229c2afa1c71eafb85747
mac503 <mac503@gmail.com>
2019-01-30 20:18:27 +0100

    Ability to exclude hidden massage types from user display.

--- includes/helpers/get_massage_types.php
+++ includes/helpers/get_massage_types.php
@@ -1,14 +1,17 @@
 <?php
-  function get_massage_types($forUpdate){
-    //TODO look up from database
+  function get_massage_types($forUpdate, $hideDeleted){
+    if($forUpdate == true) $lockString = " for update";
+    if($hideDeleted == true) $deletedString = " where allowbookings = 1";
+    global $wpdb;
+    return $wpdb->get_results("select * from {$wpdb->prefix}ss_booking_types$deletedString$lockString", ARRAY_A );
     return array(
-      array("name"=>"Massage 1 - 60 min", "length"=>60),
-      array("name"=>"Massage 2 - 60 min", "length"=>60),
-      array("name"=>"Massage 3 - 100 min", "length"=>100)
+      array("id"=>1, "name"=>"Massage 1 - 60 min", "length"=>60),
+      array("id"=>2, "name"=>"Massage 2 - 60 min", "length"=>60),
+      array("id"=>3, "name"=>"Massage 3 - 100 min", "length"=>100)
     );
   }
-  function get_massage_type_names($forUpdate){
-    $types = get_massage_types($forUpdate);
+  function get_massage_type_names($forUpdate, $hideDeleted){
+    $types = get_massage_types($forUpdate, $hideDeleted);
     return array_map("extract_name", $types);
   }
   function extract_name($type){
