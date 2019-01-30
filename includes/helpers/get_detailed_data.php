ac14baee777368c58ea6c13106db2970fc4bf670
mac503 <mac503@gmail.com>
2019-01-30 20:18:07 +0100

    Lookup massage types from db, and add ability to exclude hiddens.

--- includes/helpers/get_detailed_data.php
+++ includes/helpers/get_detailed_data.php
@@ -5,7 +5,7 @@
     global $wpdb;
     if($forUpdate == true) $lockString = " for update";
     $data["bookings"] = $wpdb->get_results("select * from {$wpdb->prefix}ss_booking_bookings$lockString", ARRAY_A );
-    $data["types"] = get_massage_types();
+    $data["types"] = get_massage_types(false, true);
     $bookingWindows = $wpdb->get_results("select * from {$wpdb->prefix}ss_booking_windows where windowsdate between '{$data["startDate"]}' and '{$data["endDate"]}'$lockString", OBJECT );
     $data["availability"] = array();
     foreach($bookingWindows as $window){
