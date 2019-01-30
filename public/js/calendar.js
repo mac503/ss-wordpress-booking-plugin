59c93b95815ca72a3fef370f20123fdc45df2ef5
mac503 <mac503@gmail.com>
2019-01-30 20:23:59 +0100

    DateBox code.

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -276,3 +276,101 @@ SabaiSaludCalendar.prototype.indexOfTime = function(time){
   var grainValue = this.display.granularity / 60;
   return (timeDec - this.display.dayStart) / grainValue;
 }
+
+
+function SabaiSaludDateBox(calendar, earliest, latest){
+  this.calendar = calendar;
+  this.earliest = earliest;
+  this.latest = latest;
+  this.earliestMonth = new Date(earliest);
+  this.earliestMonth.setDate(1);
+  this.earliestMonth.setHours(0,0,0,0);
+  this.latestMonth = new Date(latest);
+  this.latestMonth.setDate(1);
+  this.latestMonth.setHours(0,0,0,0);
+}
+
+SabaiSaludDateBox.prototype.drawBox = function(date, target, selected){
+  if(selected) selected.setHours(1,0,0,0);
+  else selected = new Date(0);
+
+  var div = document.createElement('div');
+  div.classList.add('dateBox');
+
+  this.updateBox(date, div, selected);
+
+  target.appendChild(div);
+}
+
+SabaiSaludDateBox.prototype.updateBox = function(date, div, selected){
+  if(selected == null){
+    var selectedEl = div.querySelector('.selected');
+    if(selectedEl) selected = new Date(parseInt(selectedEl.dataset.date));
+    else selected = new Date(0);
+  }
+  selected.setHours(1,0,0,0);
+
+  var year = date.getFullYear();
+  var month = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'][date.getMonth()];
+  var days = ['M','T','W','H','F','S','U'];
+  var today = new Date();
+  today.setHours(1,0,0,0);
+  var endDate = this.getEndDate(date);
+  var startDate = this.getStartDate(date);
+  var dates = [];
+  for(true; startDate<=endDate; startDate.setDate(startDate.getDate() +1)){
+    dates.push(new Date(startDate.getTime()));
+  }
+  var prevMonthDate = this.getFirstDate(date);
+  prevMonthDate.setDate(prevMonthDate.getDate() - 1);
+  prevMonthDate.setHours(0,0,0,0);
+  var nextMonthDate = this.getLastDate(date);
+  nextMonthDate.setDate(nextMonthDate.getDate() + 1);
+  nextMonthDate.setHours(0,0,0,0);
+
+  div.innerHTML = `
+  <div class='clickCover' data-events-handler='date-box-click-cover'></div>
+  <div class='shell'>
+    <div class='monthYear'><span class='changeMonth' data-date-allowed='${prevMonthDate > this.earliestMonth}' data-date='${prevMonthDate.getTime()}' data-events-handler='date-box-change-month'>&#8592;</span>${month} ${year}<span class='changeMonth' data-date='${nextMonthDate.getTime()}' data-date-allowed='${nextMonthDate <= this.latestMonth}' data-events-handler='date-box-change-month'>&#8594;</span></div>
+    <div class='days'>
+      ${days.map(x=> `<div>${x}</div>`).join('')}
+    </div>
+    <div class='dates'>
+      ${dates.map((x,i) => `${i%7 == 0 ? `<div class="dateBoxRow">` : ''}<div data-date="${x.getTime()}" class="dateChoice ${x.getMonth() == date.getMonth() ? '' : ' otherMonth'}${(x.getTime() == today.getTime()) ? ' today' : ''}${(x.getTime() == selected.getTime()) ? ' selected' : ''}" data-events-handler='date-box-date' data-date-allowed='${x>this.earliest && x<this.latest}'>${x.getDate()}</div>${i%7 == 6 ? `</div>` : ''}`).join('')}
+    </div>
+  </div>
+  `;
+}
+
+SabaiSaludDateBox.prototype.getFirstDate = function(date){
+  var tempDate = new Date(date.getTime());
+  tempDate.setDate(1);
+  tempDate.setHours(1,0,0,0);
+  return tempDate;
+}
+
+SabaiSaludDateBox.prototype.getLastDate = function(date){
+  var tempDate = new Date(date.getTime());
+  tempDate.setDate(1);
+  tempDate.setMonth(tempDate.getMonth()+1);
+  tempDate.setDate(0);
+  tempDate.setHours(1,0,0,0);
+  return tempDate;
+}
+
+SabaiSaludDateBox.prototype.getStartDate = function(date){
+  var firstDate = this.getFirstDate(date);
+  var firstDay = firstDate.getDay();
+  var startDate = new Date(firstDate.getTime());
+  startDate.setDate(startDate.getDate() - firstDay + 1);
+  return startDate;
+}
+
+SabaiSaludDateBox.prototype.getEndDate = function(date){
+  var lastDate = this.getLastDate(date);
+  var lastDay = lastDate.getDay();
+  var endDate = new Date(lastDate.getTime());
+  var toAdd = 7 - lastDay;
+  endDate.setDate(endDate.getDate() + (toAdd == 7 ? 0 : toAdd));
+  return endDate;
+}
