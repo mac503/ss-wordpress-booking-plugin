53b6da338aefb78f0284eb1efc0b32307255c03e
mac503 <mac503@gmail.com>
2019-02-19 12:02:55 +0100

    Removed placeholder data

--- includes/helpers/get_massage_types.php
+++ includes/helpers/get_massage_types.php
@@ -4,11 +4,6 @@
     if($hideDeleted == true) $deletedString = " where allowbookings = 1";
     global $wpdb;
     return $wpdb->get_results("select * from {$wpdb->prefix}ss_booking_types$deletedString$lockString", ARRAY_A );
-    return array(
-      array("id"=>1, "name"=>"Massage 1 - 60 min", "length"=>60),
-      array("id"=>2, "name"=>"Massage 2 - 60 min", "length"=>60),
-      array("id"=>3, "name"=>"Massage 3 - 100 min", "length"=>100)
-    );
   }
   function get_massage_type_names($forUpdate, $hideDeleted){
     $types = get_massage_types($forUpdate, $hideDeleted);
