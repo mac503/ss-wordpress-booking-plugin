7e1e828f2dd7ab833ad13c813e16c3dddef51ee4
mac503 <mac503@gmail.com>
2019-03-16 17:25:54 +0100

    Translate to Spanish

--- includes/helpers/validate_booking_request.php
+++ includes/helpers/validate_booking_request.php
@@ -1,15 +1,15 @@
 <?php
   function validate_booking_request($postData){
     $types = get_massage_type_names(true);
-    if(array_key_exists('date', $postData) == false || $postData['date'] == '') return array("success"=>false, "message"=>"Please select a date.");
-    if(array_key_exists('time', $postData) == false || $postData['time'] == '') return array("success"=>false, "message"=>"Please select a time.");
-    if(array_key_exists('type', $postData) == false || $postData['type'] == '') return array("success"=>false, "message"=>"Please select a type.");
-    if(in_array($postData['type'], $types) == false) return array("success"=>false, "message"=>"Please select a valid type.");
-    if(array_key_exists('name', $postData) == false || $postData['name'] == '') return array("success"=>false, "message"=>"Please enter your name.");
-    if(array_key_exists('surnames', $postData) == false || $postData['surnames'] == '') return array("success"=>false, "message"=>"Please enter your surnames.");
-    if(array_key_exists('phone', $postData) == false || $postData['phone'] == '') return array("success"=>false, "message"=>"Please enter your phone number.");
-    if(array_key_exists('email', $postData) == false || filter_var($postData['email'], FILTER_VALIDATE_EMAIL) == false) return array("success"=>false, "message"=>"Please enter a valid email.");
+    if(array_key_exists('date', $postData) == false || $postData['date'] == '') return array("success"=>false, "message"=>"Por favor, elija una fecha.");
+    if(array_key_exists('time', $postData) == false || $postData['time'] == '') return array("success"=>false, "message"=>"Por favor, seleccione una hora.");
+    if(array_key_exists('type', $postData) == false || $postData['type'] == '') return array("success"=>false, "message"=>"Por favor, seleccione un tipo.");
+    if(in_array($postData['type'], $types) == false) return array("success"=>false, "message"=>"Por favor, seleccione un tipo válido.");
+    if(array_key_exists('name', $postData) == false || $postData['name'] == '') return array("success"=>false, "message"=>"Por favor, introduzca su nombre.");
+    if(array_key_exists('surnames', $postData) == false || $postData['surnames'] == '') return array("success"=>false, "message"=>"Por favor, introduzca su apellido.");
+    if(array_key_exists('phone', $postData) == false || $postData['phone'] == '') return array("success"=>false, "message"=>"Por favor, introduzca su número de teléfono.");
+    if(array_key_exists('email', $postData) == false || filter_var($postData['email'], FILTER_VALIDATE_EMAIL) == false) return array("success"=>false, "message"=>"Por favor, introduzca un correo valido.");
     if(verify_booking_slot_valid($postData)) return array("success"=>true);
-    else return array("success"=>false, "message"=>"That booking slot is no longer available.");
+    else return array("success"=>false, "message"=>"Ese espacio de reserva ya no está disponible.");
   }
 ?>
